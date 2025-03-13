e78f5f6e23370e44a738375254e39f4857dbf94d
cyeji <98408267+cyeji@users.noreply.github.com>
2025-03-13 21:25:06 +0900

    feat : 2750. 수 정렬하기 풀이 추가

---
+++ src/main/java/com/bcdevinsights/algorithm/week3/Main3.java
@@ -0,0 +1,28 @@
+package com.bcdevinsights.algorithm.week3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * 2750. 수 정렬하기
+ */
+public class Main3 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Queue<Integer> queue = new PriorityQueue<>();
+        int s = Integer.parseInt(br.readLine());
+
+        for(int i=0;i<s;i++){
+            queue.offer(Integer.parseInt(br.readLine()));
+        }
+
+        while (!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
+    }
+}
