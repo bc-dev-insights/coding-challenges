15f8add1db346cef77382e62b8c267151ddfa222
cyeji <98408267+cyeji@users.noreply.github.com>
2025-03-16 20:33:35 +0900

    feat : 2751 수정렬하기 2 풀이 추가

---
+++ src/main/java/com/bcdevinsights/algorithm/week3/Main4.java
@@ -0,0 +1,29 @@
+package com.bcdevinsights.algorithm.week3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * 2751. 수 정렬하기 2
+ * N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+ */
+public class Main4 {
+
+    // 1,2,3,4,5
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(array);
+        for (int i = 0; i < n; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+}
